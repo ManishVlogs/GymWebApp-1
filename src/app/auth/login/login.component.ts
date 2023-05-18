58c00e073a5c487c73dc3c2c83c70cca3e540c5c
poojasawant24 <poojasawant766@gmail.com>
2023-05-19 00:00:32 +0530

    Login feature UI completion

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,25 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginServiceService } from 'src/app/services/login-service.service';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  username: string = '';
-  password: string = '';
-  constructor(private _loginServiceService: LoginServiceService) { }
+  email : string ='';
+  password : string ='';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
-  login() {
-    var login = {
-      UserName: "Pooja Sawant",
-      PassWord: "Pooja@123"
-    }
-    var Auth = this._loginServiceService.AuthenticateUser(login);
-  }
-
+ 
 }
