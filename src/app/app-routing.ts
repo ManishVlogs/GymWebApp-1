5e82260861b1e9fceb73777be654504c8a8aff00
poojasawant24 <poojasawant766@gmail.com>
2023-06-15 23:35:06 +0530

    about us implementation

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,38 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { AboutusComponent } from './module/aboutus/aboutus.component';
 import { DashboardComponent } from './module/dashboard/dashboard.component';
+import { GalleryComponent } from './module/gallery/gallery.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { SiteLayoutComponent } from './shared/components/site-layout/site-layout.component';
 
 const URLPrefix: string = 'GymManagement/';
+const DefaultRoute: string = 'GymManagement/dashboard';
 const routes: Routes = [
   { path: '', redirectTo: URLPrefix + 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', redirectTo: URLPrefix + 'dashboard', pathMatch: 'full' },
   { path: URLPrefix, redirectTo: URLPrefix + 'dashboard', pathMatch: 'full' },
-  { path: URLPrefix + 'dashboard', component: DashboardComponent },
   {
-    path: '',
-    component: SiteLayoutComponent,
+    path: 'dashboard',
+    component: AboutusComponent,
     children: [
-      {
-        path: 'dashboard', redirectTo: URLPrefix + 'dashboard', pathMatch: 'full'
-      },
+        // {
+        //   path: 'dashboard', redirectTo: URLPrefix + 'dashboard', pathMatch: 'full'
+        // },
       {
         path: URLPrefix + 'dashboard', component: DashboardComponent
       }
-      // ,
-      // {
-      //   path: 'footer', redirectTo: URLPrefix + 'footer', pathMatch: 'full'
-      // },
-      // {
-      //   path:URLPrefix+'footer',component:DashboardComponent
-      // }
+      
+     
     ]
   },
   {
-    path: '**', redirectTo: URLPrefix
+    path: '**', redirectTo: DefaultRoute
   }
 ];
 
